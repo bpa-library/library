afab9cec712a6f4de3c5add6b6d97a2f125be281
bpa-library <dontspam20@gmail.com>
2025-08-15 17:26:49 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return "Audio Book Portal - Hello World!"
+@app.route('/')
+def home():
+    return "Audio Book Portal - Hello World!"
 
-
-
-from app import app as application  # Required for Gunicorn
-
-if __name__ == "__main__":
-    application.run()
+# For WSGI compatibility
+application = app
