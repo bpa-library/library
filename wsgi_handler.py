946d5518f37bc16519f668527dcca237265429d4
bpa-library <dontspam20@gmail.com>
2025-09-18 17:46:10 +0530

    Delete wsgi_handler.py

--- wsgi_handler.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import Flask
-
-application = app
-
-def handler(event, context):
-    from serverless_wsgi import handle_request
-    return handle_request(application, event, context)
