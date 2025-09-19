69c380e00e0ad57e246dc70ac28063bdacbf1f80
bpa-library <dontspam20@gmail.com>
2025-09-19 12:03:32 +0530

    Add files via upload

---
+++ wsgi_handler.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import Flask
+
+application = app
+
+def handler(event, context):
+    from serverless_wsgi import handle_request
+    return handle_request(application, event, context)
